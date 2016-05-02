d2605f72abb4e69e8540ad1d83d557fced21dd96
tipsy <davidaase@hotmail.com>
2016-05-02 21:42:29 +0200

    Add missing slash

--- _includes/codeExamples/websocketChat/mainInit.java
+++ _includes/codeExamples/websocketChat/mainInit.java
@@ -4,7 +4,7 @@ public class Chat {
     static int nextUserNumber = 1; //Used for creating the next username
 
     public static void main(String[] args) {
-        staticFileLocation("public"); //index.html is served at localhost:4567 (default port)
+        staticFileLocation("/public"); //index.html is served at localhost:4567 (default port)
         webSocket("/chat", ChatWebSocketHandler.class);
         init();
     }
