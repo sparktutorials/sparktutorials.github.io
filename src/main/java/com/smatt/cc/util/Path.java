62d604f2f52e655701a40b8ce63b46d3b275c609
Seun Matt <smatt382@gmail.com>
2016-11-01 00:31:18 +0100

    updated DB Connection

--- src/main/java/com/smatt/cc/util/Path.java
+++ src/main/java/com/smatt/cc/util/Path.java
@@ -77,8 +77,8 @@ public class Path {
 		public static String LOCAL_DBNAME = "contacts_db";
                 public static String HOST = "127.0.0.1";
 		public static int PORT = 27017;
-                public static String HEROKU_DB_URI = "mongodb://smatt:smatt@ds135577.mlab.com:35577/heroku_zr7n290b";
-                public static String HEROKU_DB_NAME = "heroku_zr7n290b";
+                public static String HEROKU_DB_URI = "mongodb://heroku_n35m7bx6:vf99qjg9otp744biaqjtepvurd@ds011725.mlab.com:11725/heroku_n35m7bx6";
+                public static String HEROKU_DB_NAME = "heroku_n35m7bx6";
 	
 	}
         
